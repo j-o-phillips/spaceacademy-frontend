e2b13a870d9e173a1b5c173170ea1280ecb41b9d
j-o-phillips <jphillips105@googlemail.com>
2023-12-11 10:17:56 +0100

    removed index.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
-import "./index.css";
+
 import { BrowserRouter as Router } from "react-router-dom";
 import { ProfileProvider } from "./context/ProfileContext.jsx";
 
